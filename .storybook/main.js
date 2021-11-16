82261d5da0cccc88947e5d89191c9520f8e2d9ec
Jason Shannon <jwshannon@gmail.com>
2021-11-16 13:48:41 -0700

    fixed issue with main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,42 +1,12 @@
-// const path = require('path');
-// const webpack = require('webpack');
-const merge = require('webpack-merge');
-
-const maxAssetSize = 1024 * 1024;
-const minAssetSize = 30 * 1024;
-module.exports = async ({
-  config,
-  mode
-}) => {
-  return merge(config, {
-    optimization: {
-        splitChunks: {
-          chunks: 'all',
-          minSize: minAssetSize,
-          maxSize: maxAssetSize,
-        }
-      },
-      performance: {
-        maxAssetSize: maxAssetSize
-      },
-    features: {
-      postcss: false,
-    },
-    core: {
-      builder: "webpack4",
-    },
-    "stories": [
-      "../stories/**/*.stories.mdx",
-      "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    "addons": [
-      // "@storybook/addon-links",
-      "@storybook/addon-essentials",
-      "@storybook/addon-a11y",
-      "@storybook/addon-cssresources",
-      "@whitespace/storybook-addon-html",
-      "storybook-addon-pseudo-states",
-      "storybook-addon-designs"
-    ],
-  });
-};
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-a11y',
+    "@storybook/addon-cssresources"
+  ]
+}
